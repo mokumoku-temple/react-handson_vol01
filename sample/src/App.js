913eef21b8b391d2682693bda36ba63a8cb91482
takanorip <takanori0170@gmail.com>
2016-08-02 02:53:36 +0900

    README追加

--- sample/src/App.js
+++ sample/src/App.js
@@ -1,19 +1,95 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hovered: false,
+      count: 999,
+      liked: false
+    }
+  }
+  styles() {
+    return {
+      container: {
+        fontFamily: "helvetica, arial, 'hiragino kaku gothic pro', meiryo, 'ms pgothic', sans-serif",
+        fontSize: 11
+      },
+      like: {
+        display: "inline-block",
+        background: "#3b5998",
+        padding: "0px 5px",
+        borderRadius: 2,
+        color: "#ffffff",
+        cursor: "pointer",
+        float: "left",
+        height: 20,
+        lineHeight: "20px"
+      },
+      likeHover: {
+        background: "#444"
+      },
+      counterBefore: {
+        display: "block",
+        float: "left",
+        width: 6,
+        height: 6,
+        background: "#fafafa",
+        marginLeft: "-12px",
+        borderRight: 10,
+        transform: "rotate(45deg)",
+        WebkitTransform: "rotate(45deg)",
+        marginTop: 6,
+        borderLeft: "1px solid #aaa",
+        borderBottom: "1px solid #aaa"
+      },
+      counter: {
+        display: "block",
+        background: "#fafafa",
+        boxSizing: "border-box",
+        border: "1px solid #aaa",
+        float: "left",
+        padding: "0px 8px",
+        borderRadius: 2,
+        marginLeft: 8,
+        height: 20,
+        lineHeight: "20px"
+      }
+    };
+  }
+
+  onMouseEnter() {
+    this.setState({hovered: true});
+  }
+
+  onMouseLeave() {
+    this.setState({hovered: false});
+  }
+
+  onClick(){
+    this.setState({
+      count: this.state.count + (this.state.liked ? -1 : 1),
+      liked: !this.state.liked
+    });
+  }
+
   render() {
+    const styles = this.styles();
+    const likeStyle = this.state.hovered ? {...styles.like, ...styles.likeHover} : styles.like;
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <span style={styles.container}>
+        <span
+          style={likeStyle}
+          onMouseEnter={this.onMouseEnter.bind(this)}
+          onMouseLeave={this.onMouseLeave.bind(this)}
+          onClick={this.onClick.bind(this)}>{this.state.liked ? "✔" : ""}いいね！</span>
+        <span style={styles.counter}>
+          <span style={styles.counterBefore}>{" "}</span>
+          {this.state.count}
+        </span>
+      </span>
     );
   }
 }
