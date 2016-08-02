3b56f143b35097d939ff5c234265491ebc242923
takanorip <takanori0170@gmail.com>
2016-08-03 02:29:05 +0900

    CSS分離

--- sample/src/App.js
+++ sample/src/App.js
@@ -10,54 +10,6 @@ class App extends Component {
       liked: false
     }
   }
-  styles() {
-    return {
-      container: {
-        fontFamily: "helvetica, arial, 'hiragino kaku gothic pro', meiryo, 'ms pgothic', sans-serif",
-        fontSize: 11
-      },
-      like: {
-        display: "inline-block",
-        background: "#3b5998",
-        padding: "0px 5px",
-        borderRadius: 2,
-        color: "#ffffff",
-        cursor: "pointer",
-        float: "left",
-        height: 20,
-        lineHeight: "20px"
-      },
-      likeHover: {
-        background: "#444"
-      },
-      counterBefore: {
-        display: "block",
-        float: "left",
-        width: 6,
-        height: 6,
-        background: "#fafafa",
-        marginLeft: "-12px",
-        borderRight: 10,
-        transform: "rotate(45deg)",
-        WebkitTransform: "rotate(45deg)",
-        marginTop: 6,
-        borderLeft: "1px solid #aaa",
-        borderBottom: "1px solid #aaa"
-      },
-      counter: {
-        display: "block",
-        background: "#fafafa",
-        boxSizing: "border-box",
-        border: "1px solid #aaa",
-        float: "left",
-        padding: "0px 8px",
-        borderRadius: 2,
-        marginLeft: 8,
-        height: 20,
-        lineHeight: "20px"
-      }
-    };
-  }
 
   onMouseEnter() {
     this.setState({hovered: true});
@@ -75,18 +27,17 @@ class App extends Component {
   }
 
   render() {
-    const styles = this.styles();
-    const likeStyle = this.state.hovered ? {...styles.like, ...styles.likeHover} : styles.like;
+    const likeClass = this.state.hovered ? "like likeHover" : "like";
 
     return (
-      <span style={styles.container}>
+      <span className="container">
         <span
-          style={likeStyle}
           onMouseEnter={this.onMouseEnter.bind(this)}
           onMouseLeave={this.onMouseLeave.bind(this)}
-          onClick={this.onClick.bind(this)}>{this.state.liked ? "✔" : ""}いいね！</span>
-        <span style={styles.counter}>
-          <span style={styles.counterBefore}>{" "}</span>
+          onClick={this.onClick.bind(this)}
+          className={likeClass}>{this.state.liked ? "✔" : ""}いいね！</span>
+        <span className="counter">
+          <span className="counterBefore">{" "}</span>
           {this.state.count}
         </span>
       </span>
